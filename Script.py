97db1ce2981a57e582ab7f0c7af528aca3f0e266
upsettingcord <68664741+upsettingcord@users.noreply.github.com>
2021-04-15 14:02:09 +0100

    Create Script.py

---
+++ Script.py
@@ -0,0 +1,12 @@
+#Simple MadLibs Generator in python.
+
+Color = input("Input a color:  ")
+Noun = input("Enter a noun:  ")
+Verb = input("Enter a verb:  ")
+Name = input("Enter a name:  ")
+Adjective = input("Enter a adjective:"  )
+
+Print("He is", color)
+print("The", noun + "Are cool")
+print("I" + adjective, Name)
+
